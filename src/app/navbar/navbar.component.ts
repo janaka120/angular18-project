1107a3ae53a1a957d4aa555016b57d212e4c9dcc
janaka120 <janakastill@gmail.com>
2024-10-20 15:23:36 +0800

    load html template from navbar component html file

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -3,13 +3,14 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'app-navbar',
     standalone: true,
-    template: `<h1>App Navbar</h1>
-                <p>paragraph</p>
-                <button>Submit</button>`,
+    // template: `<h1>App Navbar</h1>
+    //             <p>paragraph</p>
+    //             <button>Submit</button>`,
     styles: `h1{
                 color: green;
                 background-color: #f3f3f3
-    }`
+    }`,
+    templateUrl: './navbar.component.html'
 })
 
 export class AppNavbar {
