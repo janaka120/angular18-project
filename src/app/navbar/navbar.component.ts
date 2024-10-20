b564b2821df323998387c5692e72db5d7f54444d
janaka120 <janakastill@gmail.com>
2024-10-20 12:24:36 +0800

    add inline html to navbar template

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -3,7 +3,9 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'app-navbar',
     standalone: true,
-    template: '<h1>App Navbar</h1>'
+    template: `<h1>App Navbar</h1>
+                <p>paragraph</p>
+                <button>Submit</button>`
 })
 
 export class AppNavbar {
