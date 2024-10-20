aed9c48afbbe5c00f9fdf63cfe5da73730912c0e
janaka120 <janakastill@gmail.com>
2024-10-20 12:07:04 +0800

    create navbar component

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-navbar',
+    standalone: true,
+    template: '<h1>App Navbar</h1>'
+})
+
+export class AppNavbar {
+
+}
